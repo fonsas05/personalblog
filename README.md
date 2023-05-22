91af8cba7a29cd80345390b784f51139d5c00b42
Thibault-Knobloch <thibaultknobloch@yahoo.fr>
2023-05-22 20:03:07 +0200

    tk/adlm Added basic navigation and some pages for form

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # personalblog
+
+To run the web app:
+- go to main directory (where main.py is)
+- run: flask --app main --debug run
+- website will be at URL in the output (probably: http://127.0.0.1:5000/)
+
+Next steps:
+- do not do CSS yourself: look at bootstrap (google bootstrap navbar)
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#personalblog
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# define all you pages here
+@app.route("/")
+def index():
+   return render_template("index.html")
+
+@app.route("/page2", methods=['GET', 'POST'])
+def page2():
+   # either its a gget request (USER WANTS WEBSITE PAGE)
+
+   if request.method == 'GET':
+      return render_template("page2.html", number=0, thing="ass")
+   
+   # or its a POST request (USER SUBMITTED FORM)
+   if request.method == "POST":
+      user_number = request.form['user_number']
+      user_thing = request.form['user_thing']
+      return render_template("page2.html", number=user_number, thing=user_thing)
+
+   
+
+# BS python stuff you have to do
+if __name__ == '__main__':
+   app.run()
